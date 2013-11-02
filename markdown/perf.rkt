e67c737d93264fcf8ec4f9efc9072e4f234196a2
Greg Hendershott <greghendershott@gmail.com>
2013-11-02 11:06:20 -0400

    Simple performance comparison.
    Looks like the old regexp parser is O(n) but the new parsack one is
    O(n^2). Ouch.

---
+++ markdown/perf.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(require (rename-in "main.rkt"  [read-markdown re:read-markdown])
+         (rename-in "parse.rkt" [read-markdown pr:read-markdown]))
+
+(define (random-char)
+  (let loop ()
+    (define c (integer->char (random 127)))
+    (cond [(or (char-alphabetic? c)
+               (char-numeric? c)
+               (memq c '(#\< #\> #\[ #\] #\( #\) #\_ #\newline))) c]
+          [(loop)])))
+
+(define (random-word)
+  (list->string (for/list ([i (in-range (add1 (random 10)))])
+                  (random-char))))
+
+(define (random-line)
+  (string-join (for/list ([i (in-range (+ 5 (random 15)))])
+                 (random-word))
+               " "))
+
+(define (random-doc num-lines)
+  (string-join (for/list ([n (in-range num-lines)])
+                 (random-line))
+               "\n\n"))
+
+(define (f lines)
+  (define (gc) (for ([i 3]) (collect-garbage)))
+  (define doc (random-doc lines))
+  
+  (display lines) (displayln " line random text doc")
+  
+  (gc)
+  (display "regexp: ")
+  (time (void (with-input-from-string doc re:read-markdown)))
+  
+  (gc)
+  (display "parsack: ")
+  (time (void (with-input-from-string doc pr:read-markdown)))
+  (newline))
+
+         ; RE    PR
+         ; --  ----
+(f  1)   ; 10    20
+(f 10)   ; 34   168
+(f 20)   ; 68   509
+(f 30)   ;126  1213
+(f 40)   ;154  1810
+(f 50)   ;188  3885
+
+;; The regexp parser appears to be O(n), but the parsack one looks
+;; close to O(n^2).
