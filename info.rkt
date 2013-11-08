173b3f1ff64421dac522d43e934fa62ba9ef10fb
Greg Hendershott <greghendershott@gmail.com>
2013-11-08 14:48:17 -0500

    Change parsack dep back to PNR.

--- info.rkt
+++ info.rkt
@@ -2,13 +2,4 @@
 (define version "0.7")
 (define collection 'multi)
 (define deps (list "rackjure"
-                   ;; Parsack: It's being actively developed right now
-                   ;; so specify the GitHub URL directly. (Otherwise,
-                   ;; the pkg.gracket-lang.org PNR updates only every
-                   ;; 24 hours which is too long to wait, especially e.g.
-                   ;; for Travis CI.)
-                   ;;
-                   ;; See also .travis.yml for same.
-                   "github://github.com/stchang/parsack/master"
-                   ;; "parsack"
-                   ))
+                   "parsack"))
