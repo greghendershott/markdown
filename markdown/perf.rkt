915d042c2ebf0d0c4ab267e4d6fed538176fa81f
Greg Hendershott <greghendershott@gmail.com>
2013-11-08 15:31:33 -0500

    Polish performance test. Remove random test stuff.

--- markdown/perf.rkt
+++ markdown/perf.rkt
@@ -1,59 +1,45 @@
-#lang racket
+#lang at-exp racket
 
-(require "main.rkt")
+;; A poor change to the grammar can potentially have a large
+;; performance impact. Check for that.
+;;
+;; Donwside: This might fail in an environemnt like Travis CI.
+;; Check for that case with getenv.
 
-(require racket/runtime-path)
-(define-runtime-path test.md (build-path "test" "test.md"))
+(module+ test
+  (require rackunit
+           "main.rkt")
 
-(let ()
-  (define reps 5)
-  (define doc (let ([s (file->string test.md)])
-                (string-join (for/list ([i reps])
-                               s)
-                             "\n\n")))
-  (printf "Using ~a appended ~a times, which is ~a chars and ~a lines.\n"
-          test.md
-          reps
-          (string-length doc)
-          (length (regexp-split "\n" doc)))
-  (time (void (parse-markdown doc)))
-  (newline))
+  (require racket/runtime-path)
+  (define-runtime-path test.md (build-path "test" "test.md"))
 
-(define (random-char)
-  (let loop ()
-    (define c (integer->char (random 127)))
-    (cond [(or (char-alphabetic? c)
-               (char-numeric? c)
-               (memq c '(#\< #\> #\[ #\] #\( #\) #\_ #\newline))) c]
-          [(loop)])))
+  (define (check-fast-enough?)
+    (define xs (run-times))
+    (define avg (/ (exact->inexact (apply + xs)) (length xs)))
+    (define best (apply min xs))
+    (define worst (apply max xs))
+    (displayln @~a{Times (sorted): @(string-join (map ~a (sort xs <)) ", ")
+                          Average: @avg})
+    (check-true (< avg 2750))
+    (check-true (< worst 3200))
+    ;; Check that best isn't _too_ good. If so, maybe test material
+    ;; accidentally changed.
+    (check-true (> best 2000)))
 
-(define (random-word)
-  (list->string (for/list ([i (in-range (add1 (random 10)))])
-                  (random-char))))
-
-(define (random-line)
-  (string-join (for/list ([i (in-range (+ 5 (random 15)))])
-                 (random-word))
-               " "))
-
-(define (random-doc num-lines)
-  (string-join (for/list ([n (in-range num-lines)])
-                 (random-line))
-               "\n\n"))
-
-(define (f lines)
-  (define (gc) (for ([i 3]) (collect-garbage)))
-  (define doc (random-doc lines))
+  (define (run-times)
+    (define doc-reps 5)
+    (define doc (let ([s (file->string test.md)])
+                  (string-join (for/list ([i doc-reps])
+                                 s)
+                               "\n\n")))
+    (displayln @~a{Using @test.md 
+                   appended @doc-reps times: @(string-length doc) chars and @(length (regexp-split "\n" doc)) lines.})
+    (define test-reps 5)
+    (for/list ([_ test-reps])
+      (for ([_ 3]) (collect-garbage))
+      (define-values (_ cpu real gc)
+        (time-apply parse-markdown (list doc)))
+      real))
   
-  (display lines) (displayln " line random text doc")
-  
-  (gc)
-  (time (void (parse-markdown doc)))
-  (newline))
-
-(f  1)
-(f 10)
-(f 20)
-(f 30)
-(f 40)
-(f 50)
+  (unless (getenv "TRAVIS")
+    (check-fast-enough?)))
