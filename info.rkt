3852e0de4dd31e2bdb39d56f45e19edfdfab9397
Greg Hendershott <greghendershott@gmail.com>
2013-11-08 07:58:31 -0500

    Switch parsack dep from PNR to GitHub.
    Just temporarily until Parsack dev settles down. Because the
    pkg.racket-lang.org PNR updates only daily -- too slowly.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,12 @@
 #lang setup/infotab
 (define version "0.7")
 (define collection 'multi)
-(define deps (list "rackjure" "parsack"))
+(define deps (list "rackjure"
+                   ;; Parsack: It's being actively developed right now
+                   ;; so specify the GitHub URL directly. (Otherwise,
+                   ;; the pkg.gracket-lang.org PNR updates only every
+                   ;; 24 hours which is too long to wait, especially e.g.
+                   ;; for Travis CI.)
+                   "github://github.com/stchang/parsack/master"
+                   ;; "parsack"
+                   ))
