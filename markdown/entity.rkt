8f29872c9ea44823ebb48fc4d4c1aabb4f1e3768
Greg Hendershott <greghendershott@gmail.com>
2014-07-11 16:27:39 -0400

    Refactor entity grammar for common prefixes

--- markdown/entity.rkt
+++ markdown/entity.rkt
@@ -5,33 +5,33 @@
 (provide $entity)
 
 (define $char-entity/dec
-  (try (pdo (char #\&)
-            (char #\#)
-            (x <- (many1 $digit))
+  (try (pdo (x <- (many1 $digit))
             (char #\;)
             (return (string->number (list->string x) 10)))))
 
 (define $char-entity/hex
-  (try (pdo (char #\&)
-            (char #\#)
-            (<or> (char #\x)
+  (try (pdo (<or> (char #\x)
                   (char #\X))
             (x <- (many1 $hexDigit))
             (char #\;)
             (return (string->number (list->string x) 16)))))
 
+(define $char-entity
+  (>> (char #\#)
+      (<or> $char-entity/dec
+            $char-entity/hex)))
+
 (define $sym-entity
-  (try (pdo (char #\&)
-            (x <- (many1 (<or> $letter $digit)))
+  (try (pdo (x <- (many1 (<or> $letter $digit)))
             (char #\;)
             (return (string->symbol (list->string x))))))
 
 (define $not-entity
-  (pdo (char #\&)
-       (return "&"))) ;; not 'amp -- act like xexpr
+  (return "&")) ;; not 'amp -- act like xexpr
 
 (define $entity
-  (<or> $char-entity/dec
-        $char-entity/hex
-        $sym-entity
-        $not-entity))
+  (>> (char #\&)
+      (<or> $char-entity
+            $char-entity
+            $sym-entity
+            $not-entity)))
