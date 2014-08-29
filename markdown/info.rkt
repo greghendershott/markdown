f59cb0d1de1c7dcb7f43befbc055065d32c76f09
Leif Andersen <leif@leifandersen.net>
2014-08-29 15:53:42 -0400

    Markdown is a raco command rather than a standalone executable.

--- markdown/info.rkt
+++ markdown/info.rkt
@@ -1,4 +1,3 @@
 #lang setup/infotab
 
-(define racket-launcher-names (list "Markdown"))
-(define racket-launcher-libraries (list "main.rkt"))
+(define raco-commands '(("markdown" (submod markdown main) "run markdown" #f)))
